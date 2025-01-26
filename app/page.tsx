9352da8b2538f6c4dd7a5c902413e88ad0c2b4e5
LearnThroughStoriesCode <tyler@learnthroughstories.com>
2025-01-26 11:36:47 -0500

    Add Substack link button to upper right corner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <a href="https://substack.com/@tylervschwartz" className={styles.substackLink} target="_blank" rel="noopener noreferrer">
+        <Image
+          src="/images/substack.jpg"
+          alt="Follow on Substack"
+          width={50}
+          height={50}
+          className={styles.substackIcon}
+        />
+      </a>
       <div className={styles.gameTitle}>
         <h1>Are You Smarter<br />Than Mr. Fluffbutt?</h1>
       </div>
