0cc4b5b72c628e085025ea7a034f6a16d90b89ba
LearnThroughStoriesCode <tyler@learnthroughstories.com>
2025-01-31 13:27:11 -0500

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import BackgroundPattern from './components/BackgroundPattern'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0B43NL2HR2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-0B43NL2HR2');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <BackgroundPattern pattern="paper" tone="warm">
           {children}
